2d8a96e225d4ee11661e83bc60d8ca828ea7ab1b
chekos <chekos.data@gmail.com>
2019-03-04 10:07:11 -0800

    Create install.R

---
+++ install.R
@@ -0,0 +1,2 @@
+packages (c('dplyr', 'tidyr', 'gapminder', 'ggplot2',  'ggalt', 'forcats', 'R.utils', 'png', 'grid', 'ggpubr', 'scales'), repos = "http://cran.us.r-project.org")
+devtools::install_github('bbc/bbplot')
